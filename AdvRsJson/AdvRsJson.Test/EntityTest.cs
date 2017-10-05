c96c5c4f7d9f585d578e86990943d0bf1df49c2e
Eric Sambach <30898270+EricSambach@users.noreply.github.com>
2017-10-05 13:23:32 +0200

    Added Test project

---
+++ AdvRsJson/AdvRsJson.Test/EntityTest.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace AdvRsJson.Test
+{
+    [TestClass]
+    public class EntityTest
+    {
+        [TestMethod]
+        public void TestMethod()
+        {
+            AdvRsJson.RsEntity entity = new RsEntity();
+            entity.AddAttribute("attributeName", "attributeValue");
+            entity.AddProperty("projectName", "projectValue");
+
+            string json = Newtonsoft.Json.JsonConvert.SerializeObject(entity);
+
+        }
+    }
+}
