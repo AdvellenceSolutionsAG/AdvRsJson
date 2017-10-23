6cb9facaf8e9738fc056dc0b7ab0bde90bff9644
RivasAl <alberto.rivas@advellence.com>
2017-10-23 10:27:29 +0200

    Add new Version with complex and relationships

---
+++ AdvRsJson/AdvRsJson/RsGroup.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AdvRsJson
+{
+    public class RsGroup
+    {
+        public List<RsAttribute> group = new List<RsAttribute>();
+
+        public RsGroup(object value, string id = null, string locale = RsConstants.VALUE_LOCALE, string source = RsConstants.VALUE_SOURCE)
+        {
+            group.Add(new RsAttribute(value, id, locale, source));
+        }
+
+        public RsGroup()
+        { }
+
+        public void AddChildtoGroup(object value)
+        {
+            group.Add(new RsAttribute(value));
+        }
+
+    }
+}
