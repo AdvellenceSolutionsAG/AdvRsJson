3c83660d58ecf62430749c560e925f409389be9c
RivasAl <alberto.rivas@advellence.com>
2017-11-14 09:52:48 +0100

    Add Base and Blob handling

---
+++ SyncTool/AdvRsJsonSync/AdvRsJson/RsBase.cs
@@ -0,0 +1,35 @@
+﻿using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AdvRsJson
+{
+    public class RsBase
+    {
+
+        public RsBase() {
+            Data = new RsData();
+        }
+
+        [JsonProperty("id", NullValueHandling = NullValueHandling.Ignore)]
+        public string Id { get; set; }
+
+        [JsonProperty("name", NullValueHandling = NullValueHandling.Ignore)]
+        public string Name { get; set; }
+
+        [JsonProperty("version", NullValueHandling = NullValueHandling.Ignore)]
+        public string Version { get; set; }
+
+        [JsonProperty("type", NullValueHandling = NullValueHandling.Ignore)]
+        public string Type { get; set; }
+
+        [JsonProperty("data", NullValueHandling = NullValueHandling.Ignore)]
+        public RsData Data { get; private set; }
+
+        [JsonProperty("tags", NullValueHandling = NullValueHandling.Ignore)]
+        public string[] Tags { get; set; }
+    }
+}
