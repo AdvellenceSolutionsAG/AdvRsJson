8ff1a8366558a43c8b7e8918b06beb39423a401c
Eric Sambach <30898270+EricSambach@users.noreply.github.com>
2018-06-18 09:31:42 +0200

    Added RsAction support to RsValue (needed for action = "delete")

---
+++ SyncTool/AdvRsJsonSync/AdvRsJson/RsAction.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AdvRsJson
+{
+    /// <summary>
+    /// type-safe-enum pattern.
+    /// 
+    /// Currently only the "delete" action needs to be supported.
+    /// </summary>
+    public sealed class RsAction
+    {
+        private string m_value;
+        private static readonly Dictionary<string, RsAction> m_instance = new Dictionary<string, RsAction>();
+
+        public static readonly RsAction DELETE = new RsAction("delete");
+        //public static readonly RsAction CREATE = new RsAction("create");
+        //public static readonly RsAction IGNORE = new RsAction("ignore");
+
+        private RsAction( string value )
+        {
+            m_value = value;
+            m_instance[value] = this;
+        }
+
+        public override string ToString()
+        {
+            return m_value;
+        }
+
+        public static explicit operator RsAction(string str)
+        {
+            RsAction result;
+            if (m_instance.TryGetValue(str, out result))
+                return result;
+            else
+                throw new InvalidCastException();
+        }
+    }
+}
--- SyncTool/AdvRsJsonSync/AdvRsJson/RsValue.cs
+++ SyncTool/AdvRsJsonSync/AdvRsJson/RsValue.cs
@@ -4,8 +4,22 @@ namespace AdvRsJson
 {
     public class RsValue
     {
+        /// <summary>
+        /// Currently the only action used is "delete", which is why (by default) it is set to <code>null</code> in all other constructors.
+        /// </summary>
+        /// <param name="action">The action for the given value.</param>
+        public RsValue(RsAction action, string val = null, string loc = null, string sou = null, string unitofm = null)
+        {
+            this.action = action;
+            this.Value = val;
+            this.Source = sou;
+            this.Locale = loc;
+            this.Uom = unitofm;
+        }
 
-        public RsValue(string val, string sou, string loc) {
+        public RsValue(string val, string sou, string loc)
+        {
+            action = null;
             this.Value = val;
             this.Source = sou;
             this.Locale = loc;
@@ -13,12 +27,14 @@ namespace AdvRsJson
 
         public RsValue(string val, string loc)
         {
+            action = null;
             this.Value = val;
             this.Locale = loc;
         }
 
         public RsValue(string val, string loc, string sou, string unitofm)
         {
+            action = null;
             this.Value = val;
             this.Source = sou;
             this.Locale = loc;
@@ -26,20 +42,21 @@ namespace AdvRsJson
         }
 
         public RsValue()
-        { }
-   
+        {
+            action = null;
+        }
 
-        [JsonProperty("value", NullValueHandling = NullValueHandling.Ignore, Order = 1)]
+        [JsonProperty("action", NullValueHandling = NullValueHandling.Ignore, Order = 1)]
+        public RsAction action { get; set; }
+        [JsonProperty("value", NullValueHandling = NullValueHandling.Ignore, Order = 2)]
         public string Value { get; set; }
-        [JsonProperty("source", NullValueHandling = NullValueHandling.Ignore, Order = 2)]
+        [JsonProperty("source", NullValueHandling = NullValueHandling.Ignore, Order = 3)]
         public string Source { get; set; }
-        [JsonProperty("locale", NullValueHandling = NullValueHandling.Ignore, Order = 3)]
+        [JsonProperty("locale", NullValueHandling = NullValueHandling.Ignore, Order = 4)]
         public string Locale { get; set; }
-        [JsonProperty("uom", NullValueHandling = NullValueHandling.Ignore, Order = 4)]
+        [JsonProperty("uom", NullValueHandling = NullValueHandling.Ignore, Order = 5)]
         public string Uom { get; set; }
-        //[JsonProperty("timestamp", NullValueHandling = NullValueHandling.Ignore, Order = 4)]
+        //[JsonProperty("timestamp", NullValueHandling = NullValueHandling.Ignore, Order = 6)]
         //public float Timestamp { get; set; }
-
-
     }
 }
